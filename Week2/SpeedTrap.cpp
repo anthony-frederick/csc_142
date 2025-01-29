0e79125eaba60b44ab7faa5309b19e960c272d61
anthony-frederick <anthony.frederick001@albright.edu>
2025-01-29 12:47:36 -0500

    Add files via upload

--- Week2/SpeedTrap.cpp
+++ Week2/SpeedTrap.cpp
@@ -9,11 +9,13 @@ int main(){
     int speed_limit;
     double driving_speed;
 
+    // Ask about the speed limit and what you are driving
     std::cout<<"Input the speed limit of the road you are on: ";
     std::cin>>speed_limit;
     std::cout<<"Input the speed that you were driving at: ";
     std::cin>>driving_speed;
 
+    // Check if you are speeding
     if(driving_speed >= speed_limit)
     std::cout<<"You are speeding you maniac!" <<std::endl;
 
