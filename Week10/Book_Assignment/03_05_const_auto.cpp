3070e933ceb94c86c414f8e10a02712cc6969d9e
anthony-frederick <anthony.frederick001@albright.edu>
2025-03-27 18:27:32 -0400

    Add files via upload

---
+++ Week10/Book_Assignment/03_05_const_auto.cpp
@@ -0,0 +1,8 @@
+/* In the listing for 2-20 you can use const instead of int
+because the year won't change when you are outputing the date.
+
+
+You can put auto in mostly all of them just because of how
+auto functions but it is a lot more efficient to acually use the
+int or float so that it doesn't draw out the right thing.
+*/
