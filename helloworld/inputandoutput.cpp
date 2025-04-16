a4e6b613656fb5c72833ca02a8209cbe49ca15ec
anthony-frederick <anthony.frederick001@albright.edu>
2025-04-16 12:06:17 -0400

    Delete helloworld directory

--- helloworld/inputandoutput.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-
-    std::string desc;
-    std::cout <<"Please enter a description of your car: ";
-    std::getline(std::cin, desc);
-    std::cout <<"Great Ride!" << std::endl;
-}
